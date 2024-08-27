5c86028d0c3a808a1dd251afe485e6db87634de5
samanemkul <nemkulsama1@gmail.com>
2024-08-27 09:02:38 +0545

    added

---
+++ django blog/blog/main/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .import views
+
+urlpatterns = [
+
+    
+    path('accounts/signup/', views.signup_view, name="signup_page"),
+    path('accounts/login/', views.login_view, name="login_page"),
+    path('accounts/logout/', views.logout_view, name="logout_page"),
+
+
+    
+    path('', views.home, name="home_page"),
+]
